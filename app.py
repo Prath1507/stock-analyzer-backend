856130326b12891bfd85d056a87ba48c24bbeb0b
Prath1507 <117733234+Prath1507@users.noreply.github.com>
2026-05-28 19:22:20 +0530

    fix CORS for production frontend

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # =========================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["  https://stock-analyzer-frontend-ten.vercel.app",
+    allow_origins=["https://stock-analyzer-frontend-ten.vercel.app",
         "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
